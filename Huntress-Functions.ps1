b3982ac3321640c0372be5c26616cc17609cb64f
PhoenixC46 <44898938+PhoenixC46@users.noreply.github.com>
2025-09-17 10:19:15 +0100

    ListSignals function created with params
    itemLimit
    entityType
    dateFrom
    dateTo
    orgId

--- Huntress-Functions.ps1
+++ Huntress-Functions.ps1
@@ -2,9 +2,14 @@ $Path = "D:\Huntress\"
 $AuthInfo = [pscustomobject](Get-Content $Path"Huntress.txt" -Raw | ConvertFrom-StringData)
 $HKey = $AuthInfo.h1
 $HTok = $AuthInfo.h2
+$OnError = "SilentlyContinue"
 Function Connect-Huntress {
     param ( 
-    [parameter(Mandatory = $true)][string]$data
+        [parameter(Mandatory = $true)][string]$data,
+        [parameter(Mandatory = $false)][Int32]$itemLimit,
+        [parameter(Mandatory = $false)][string]$entityType,
+        [parameter(Mandatory = $false)][datetime]$dateFrom,
+        [parameter(Mandatory = $false)][datetime]$dateTo
     )
     $apiKey = $HKey
     $apiSecret = $HTok
@@ -12,9 +17,8 @@ Function Connect-Huntress {
     $apiAuthEnc = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($apiAuth))
     $headers = @{"Authorization" = "Basic $apiAuthEnc"}
     $uriBase = 'https://api.huntress.io/v1/'
-    $uri = $uriBase + $data
-    $apiUri = $uri
-
+    $apiUri = -join($uriBase, $data)
+    
     try {
         $response = Invoke-RestMethod -Uri $apiUri -Headers $headers -Method Get
         Write-Host "Successfully authenticated and received payload:"
@@ -52,9 +56,42 @@ Function Huntress-GetAgent {
     $HuntressData = Connect-Huntress -data agents/$agentId
 return $HuntressData}
 
-Function Huntress-Signals {
-    $HuntressData = Connect-Huntress -data signals
-return $HuntressData}
+Function Huntress-ListSignals {
+    param ( 
+        [parameter(Mandatory = $false)][Int32]$itemLimit = 10,
+        [parameter(Mandatory = $false)][string]$entityType,
+        [parameter(Mandatory = $false)][datetime]$dateFrom,
+        [parameter(Mandatory = $false)][datetime]$dateTo,
+        [parameter(Mandatory = $false)][Int32]$orgId
+    )
+    $entityType = "agent"
+    $itemLimit = 2
+    Clear-Variable dataStr -Force -ErrorAction $OnError
+    $dataStr = "signals"
+    If($itemLimit -ge 1) {
+        Write-Host "LimitValue:"$itemLimit
+        $dataStr = -join($dataStr, "?limit=" + $itemLimit)}
+    If(!$types) {
+        Write-Host "TypeValue:"$entityType
+        $dataStr = -join($dataStr, "&entity_type=" + $entityType)}
+    If($null -ne $dateFrom) {
+        try{
+            [datetime]::parseexact(($dateFrom.ToString('yyyy-MM-ddThh:mm:ssZ')), 'yyyy-MM-ddThh:mm:ssZ', $null)
+            $dataStr = -join($dataStr, '&investigated_at_min=' + ($dateFrom.ToString('yyyy-MM-ddThh:mm:ssZ')) -replace ":","%3A")}
+        catch{Write-Host 'Invalid Start Date/Time Format, must be entered as: "yyyy-MM-ddThh:mm:ssZ" (wrapped in "double-quotes")'}
+    }
+    If($null -ne $dateTo) {
+        try{
+            [datetime]::parseexact(($dateTo.ToString('yyyy-MM-ddThh:mm:ssZ')), 'yyyy-MM-ddThh:mm:ssZ', $null)
+            $dataStr = -join($dataStr, '&investigated_at_max=' + ($dateTo.ToString('yyyy-MM-ddThh:mm:ssZ')) -replace ":","%3A")}
+        catch{Write-Host 'Invalid End Date/Time Format, must be entered as: "yyyy-MM-ddThh:mm:ssZ" (wrapped in "double-quotes")'}
+    }
+    If($orgId -ge 1) {
+        Write-Host "orgId:"$orgId
+        $dataStr = -join($dataStr, "&organization_id=" + $orgId)}
+    $HuntressData = Connect-Huntress -data $dataStr
+return $HuntressData.signals}
+
 Function Huntress-GetSignal {
     param ( 
     [parameter(Mandatory = $true)][Int32]$signalId
@@ -64,10 +101,6 @@ return $HuntressData}
 
 Function Huntress-BillingReports {
     $HuntressData = Connect-Huntress -data billing_reports
-return $HuntressData}
-
-Function Huntress-SummaryReports {
-    $HuntressData = Connect-Huntress -data reports
 return $HuntressData}
 
 Function Huntress-SummaryReports {
